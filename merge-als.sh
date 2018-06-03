4e7dd961d92a6877866695183d175ca8e46dc805
jhg14 <jonnygoffwhite@gmail.com>
2018-06-03 14:52:07 +0100

    Amend file path in bash merge script

--- merge-als.sh
+++ merge-als.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mkdir mergetemp
+mkdir .merge/mergetemp
 gzip -cd $1 > .merge/mergetemp/base.xml
 gzip -cd $2 > .merge/mergetemp/ours.xml
 gzip -cd $3 > .merge/mergetemp/theirs.xml
 
 python3 .merge/merge.py .merge/mergetemp/base.xml .merge/mergetemp/ours.xml .merge/mergetemp/theirs.xml .merge/mergetemp/merged.xml
-gzip -c .merge/mergetemp/merged.xml > $4
+gzip -c .merge/mergetemp/merged.xml > $2
 
 rm -r .merge/mergetemp
